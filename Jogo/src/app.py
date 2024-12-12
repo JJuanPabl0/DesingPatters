19b7bf29d3758a3863cf439b3d0a1c544f0cc358
Juan Pablo <juansilverio865@gmail.com>
2024-12-12 17:21:44 -0300

    update: alterando o codigo da pasta
    docs: adicionando novos conteudo ao codigo, mais dois tipos de desing... Composite e bridge, que antes ja tinham sido colocado o Facede e adapter

--- Jogo/src/app.py
+++ Jogo/src/app.py
@@ -15,116 +15,124 @@ PRETO = (0, 0, 0)
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Ping Pong")
 
-# Raquete
-raquete_largura = 10
-raquete_altura = 100
-velocidade_raquete = 6
-
-# Posição inicial das raquetes
-raquete_esquerda_y = (ALTURA // 2) - (raquete_altura // 2)
-raquete_direita_y = (ALTURA // 2) - (raquete_altura // 2)
-
-# Bola
-bola_raio = 10
-bola_x = LARGURA // 2
-bola_y = ALTURA // 2
-velocidade_bola_x = 4
-velocidade_bola_y = 4
-
-# Pontuação
-pontuacao_esquerda = 0
-pontuacao_direita = 0
-
 # Relógio
 relogio = pygame.time.Clock()
 
-# Adapter para entrada
-class InputAdapter:
+# Fonte para o placar
+fonte = pygame.font.Font(None, 74)
+
+# Padrão Bridge: Interface para renderização
+class Renderizador:
+    def renderizar(self, tela, elemento):
+        raise NotImplementedError("Deve ser implementado pela subclasse")
+
+# Implementação concreta do Renderizador
+class Renderizador2D(Renderizador):
+    def renderizar(self, tela, elemento):
+        elemento.desenhar(tela)
+
+# Padrão Composite: Interface comum para elementos do jogo
+class ElementoJogo:
+    def atualizar(self):
+        pass
+
+    def desenhar(self, tela):
+        pass
+
+# Raquete como um Leaf do Composite
+class Raquete(ElementoJogo):
+    def __init__(self, x, y, largura, altura, velocidade):
+        self.x = x
+        self.y = y
+        self.largura = largura
+        self.altura = altura
+        self.velocidade = velocidade
+
+    def mover(self, direcao, altura_maxima):
+        if direcao == "cima" and self.y > 0:
+            self.y -= self.velocidade
+        elif direcao == "baixo" and self.y < altura_maxima - self.altura:
+            self.y += self.velocidade
+
+    def desenhar(self, tela):
+        pygame.draw.rect(tela, BRANCO, (self.x, self.y, self.largura, self.altura))
+
+# Bola como outro Leaf do Composite
+class Bola(ElementoJogo):
+    def __init__(self, x, y, raio, velocidade_x, velocidade_y):
+        self.x = x
+        self.y = y
+        self.raio = raio
+        self.velocidade_x = velocidade_x
+        self.velocidade_y = velocidade_y
+        self.velocidade_inicial = (velocidade_x, velocidade_y)
+
+    def atualizar(self, largura, altura, raquete_esq, raquete_dir, placar):
+        self.x += self.velocidade_x
+        self.y += self.velocidade_y
+
+        # Colisão com paredes
+        if self.y - self.raio <= 0 or self.y + self.raio >= altura:
+            self.velocidade_y *= -1
+
+        # Colisão com raquetes
+        if (self.x - self.raio <= raquete_esq.x + raquete_esq.largura and
+            raquete_esq.y <= self.y <= raquete_esq.y + raquete_esq.altura):
+            self.velocidade_x *= -1
+
+        if (self.x + self.raio >= raquete_dir.x and
+            raquete_dir.y <= self.y <= raquete_dir.y + raquete_dir.altura):
+            self.velocidade_x *= -1
+
+        # Verifica se alguém pontuou
+        if self.x < 0:
+            placar[1] += 1
+            self.resetar_posicao(largura, altura)
+        elif self.x > largura:
+            placar[0] += 1
+            self.resetar_posicao(largura, altura)
+
+    def resetar_posicao(self, largura, altura):
+        self.x = largura // 2
+        self.y = altura // 2
+        self.velocidade_x, self.velocidade_y = self.velocidade_inicial
+
+    def desenhar(self, tela):
+        pygame.draw.ellipse(tela, BRANCO, (self.x - self.raio, self.y - self.raio, self.raio * 2, self.raio * 2))
+
+# Composite principal que agrega elementos do jogo
+class ComponenteJogo(ElementoJogo):
     def __init__(self):
-        self.teclas = {}
+        self.elementos = []
 
-    def mapear_teclas(self, acao, tecla):
-        self.teclas[acao] = tecla
+    def adicionar(self, elemento):
+        self.elementos.append(elemento)
 
-    def acao_press(self, acao, teclas_pressionadas):
-        return teclas_pressionadas[self.teclas.get(acao, None)]
+    def atualizar(self, largura, altura, placar):
+        for elemento in self.elementos:
+            if isinstance(elemento, Bola):
+                elemento.atualizar(largura, altura, self.elementos[0], self.elementos[1], placar)
 
-# Criar instância do adaptador
-input_adapter = InputAdapter()
-input_adapter.mapear_teclas("raquete_esquerda_cima", pygame.K_w)
-input_adapter.mapear_teclas("raquete_esquerda_baixo", pygame.K_s)
-input_adapter.mapear_teclas("raquete_direita_cima", pygame.K_UP)
-input_adapter.mapear_teclas("raquete_direita_baixo", pygame.K_DOWN)
+    def desenhar(self, tela):
+        for elemento in self.elementos:
+            elemento.desenhar(tela)
 
-# Facade para gerenciamento do jogo
-class PingPongFacade:
-    def __init__(self):
-        self.raquete_esquerda_y = raquete_esquerda_y
-        self.raquete_direita_y = raquete_direita_y
-        self.bola_x = bola_x
-        self.bola_y = bola_y
-        self.velocidade_bola_x = velocidade_bola_x
-        self.velocidade_bola_y = velocidade_bola_y
-        self.pontuacao_esquerda = pontuacao_esquerda
-        self.pontuacao_direita = pontuacao_direita
+# Criar elementos do jogo
+raquete_esquerda = Raquete(10, ALTURA // 2 - 50, 10, 100, 6)
+raquete_direita = Raquete(LARGURA - 20, ALTURA // 2 - 50, 10, 100, 6)
+bola = Bola(LARGURA // 2, ALTURA // 2, 10, 4, 4)
 
-    def mover_raquete(self, lado, direcao):
-        if lado == "esquerda":
-            if direcao == "cima" and self.raquete_esquerda_y > 0:
-                self.raquete_esquerda_y -= velocidade_raquete
-            elif direcao == "baixo" and self.raquete_esquerda_y < ALTURA - raquete_altura:
-                self.raquete_esquerda_y += velocidade_raquete
-        elif lado == "direita":
-            if direcao == "cima" and self.raquete_direita_y > 0:
-                self.raquete_direita_y -= velocidade_raquete
-            elif direcao == "baixo" and self.raquete_direita_y < ALTURA - raquete_altura:
-                self.raquete_direita_y += velocidade_raquete
+# Componente principal do jogo
+jogo = ComponenteJogo()
+jogo.adicionar(raquete_esquerda)
+jogo.adicionar(raquete_direita)
+jogo.adicionar(bola)
 
-    def mover_bola(self):
-        self.bola_x += self.velocidade_bola_x
-        self.bola_y += self.velocidade_bola_y
+# Renderizador
+renderizador = Renderizador2D()
 
-        # Colisão com as paredes
-        if self.bola_y - bola_raio <= 0 or self.bola_y + bola_raio >= ALTURA:
-            self.velocidade_bola_y *= -1
-
-        # Colisão com as raquetes
-        if (self.bola_x - bola_raio <= raquete_largura and
-            self.raquete_esquerda_y <= self.bola_y <= self.raquete_esquerda_y + raquete_altura):
-            self.velocidade_bola_x *= -1
-
-        if (self.bola_x + bola_raio >= LARGURA - raquete_largura and
-            self.raquete_direita_y <= self.bola_y <= self.raquete_direita_y + raquete_altura):
-            self.velocidade_bola_x *= -1
-
-        # Verificar pontuação
-        if self.bola_x < 0:
-            self.pontuacao_direita += 1
-            self.resetar_bola()
-        if self.bola_x > LARGURA:
-            self.pontuacao_esquerda += 1
-            self.resetar_bola()
-
-    def resetar_bola(self):
-        self.bola_x = LARGURA // 2
-        self.bola_y = ALTURA // 2
-        self.velocidade_bola_x *= -1
-
-    def desenhar(self):
-        tela.fill(PRETO)
-        pygame.draw.rect(tela, BRANCO, (10, self.raquete_esquerda_y, raquete_largura, raquete_altura))
-        pygame.draw.rect(tela, BRANCO, (LARGURA - 20, self.raquete_direita_y, raquete_largura, raquete_altura))
-        pygame.draw.ellipse(tela, BRANCO, (self.bola_x - bola_raio, self.bola_y - bola_raio, bola_raio * 2, bola_raio * 2))
-        pygame.draw.line(tela, BRANCO, (LARGURA // 2, 0), (LARGURA // 2, ALTURA), 1)
-
-        # Desenhar a pontuação
-        fonte = pygame.font.Font(None, 36)
-        texto = fonte.render(f"{self.pontuacao_esquerda} - {self.pontuacao_direita}", True, BRANCO)
-        tela.blit(texto, (LARGURA // 2 - texto.get_width() // 2, 10))
-        pygame.display.flip()
-
-# Criar instância da facade
-jogo = PingPongFacade()
+# Placar
+placar = [0, 0]
 
 # Loop principal do jogo
 rodando = True
@@ -135,20 +143,29 @@ while rodando:
 
     # Movimento das raquetes
     teclas = pygame.key.get_pressed()
-    if input_adapter.acao_press("raquete_esquerda_cima", teclas):
-        jogo.mover_raquete("esquerda", "cima")
-    if input_adapter.acao_press("raquete_esquerda_baixo", teclas):
-        jogo.mover_raquete("esquerda", "baixo")
-    if input_adapter.acao_press("raquete_direita_cima", teclas):
-        jogo.mover_raquete("direita", "cima")
-    if input_adapter.acao_press("raquete_direita_baixo", teclas):
-        jogo.mover_raquete("direita", "baixo")
+    if teclas[pygame.K_w]:
+        raquete_esquerda.mover("cima", ALTURA)
+    if teclas[pygame.K_s]:
+        raquete_esquerda.mover("baixo", ALTURA)
+    if teclas[pygame.K_UP]:
+        raquete_direita.mover("cima", ALTURA)
+    if teclas[pygame.K_DOWN]:
+        raquete_direita.mover("baixo", ALTURA)
 
-    # Movimento da bola
-    jogo.mover_bola()
+    # Atualizar elementos do jogo
+    jogo.atualizar(LARGURA, ALTURA, placar)
 
-    # Desenhar a tela
-    jogo.desenhar()
+    # Renderizar elementos
+    tela.fill(PRETO)
+    renderizador.renderizar(tela, jogo)
+
+    # Renderizar o placar
+    texto_esq = fonte.render(str(placar[0]), True, BRANCO)
+    texto_dir = fonte.render(str(placar[1]), True, BRANCO)
+    tela.blit(texto_esq, (LARGURA // 4, 20))
+    tela.blit(texto_dir, (3 * LARGURA // 4, 20))
+
+    pygame.display.flip()
 
     # Controlar a taxa de quadros
     relogio.tick(60)
